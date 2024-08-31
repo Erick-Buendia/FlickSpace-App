fb3dc82f6062df26387198c2efa0f7bc3d250510
Erick-Buendia <ericandresbuendiarosas@gmail.com>
2024-08-31 17:50:04 -0600

    Add view FavoritesFragament

--- app/src/main/java/com/erick/buendia/appmovie/ui/view/DetailMovieActivity.kt
+++ app/src/main/java/com/erick/buendia/appmovie/ui/view/DetailMovieActivity.kt
@@ -1,13 +1,12 @@
 package com.erick.buendia.appmovie.ui.view
 
-import android.annotation.SuppressLint
-import android.content.Intent
-import android.os.Build
+
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.bumptech.glide.Glide
 
 import com.erick.buendia.appmovie.R
@@ -22,7 +21,7 @@ class DetailMovieActivity : AppCompatActivity() {
     }
 
     private   lateinit  var binding: ActivityDetailMovieBinding
-
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailMovieBinding.inflate(layoutInflater)
@@ -32,28 +31,16 @@ class DetailMovieActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowTitleEnabled(false)
-    //        supportActionBar.(
-    //            MaterialColors.getColor(appBarLayout, com.google.android.material.R.attr.colorSurface))
-
-    //      val movie = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-    //          intent.getParcelableExtra(DETAIL_MOVIE, T::class.java)
-    //
-    //      } else {
         val movie =   intent.getParcelableExtra<Result>(DETAIL_MOVIE)
-//      }
-
         if (movie != null) {
             binding.detailTitle.text = movie.title
             binding.detailDate.text = movie.release_date
             binding.detailDescription.text = movie.overview
             Glide.with(binding.detailImage.context).load("https://image.tmdb.org/t/p/w780${movie.poster_path}").into(binding.detailImage)
-
         }
-
-
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return true
+      return super.onSupportNavigateUp()
     }
 }
